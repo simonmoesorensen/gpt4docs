0adee1615784028bfaf14c574a54cfc4f7c14940
Simon Moe Sørensen <sso@2021.ai>
2023-08-04 11:24:25 +0200

    🔵 test(conftest): Update fixtures to reference different paths and clean up after testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,30 @@
 import pytest
-from autodocs import File
+from autodocs import File, Project
+from pathlib import Path
+import os
+
+TEST_DATA = Path("tests/data/")
 
 
 @pytest.fixture
 def file():
-    return File("tests/data/test.py")
+    return File(TEST_DATA / "test.py")
+
+
+@pytest.fixture
+def project_root():
+    return TEST_DATA / "test_package"
+
+
+@pytest.fixture
+def project(project_root):
+    return Project(project_root)
+
+
+# Clean up all files after test
+def pytest_sessionfinish(session, exitstatus):
+    # Remove all .new.py files
+    new_files = TEST_DATA.rglob("*.new.py")
+
+    for file in new_files:
+        os.remove(file)
