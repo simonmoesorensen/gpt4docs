216ac7d980686813479209c0cde2e88615574d63
Simon Moe Sørensen <sso@2021.ai>
2023-08-14 10:46:03 +0200

    ✨ feat(logging): Add debug filehandler

--- gpt4docs/logger_config.py
+++ gpt4docs/logger_config.py
@@ -1,16 +1,28 @@
 import logging
 
-# LOGGING_LEVEL = logging.DEBUG
 LOGGING_LEVEL = logging.INFO
-
-logging.basicConfig(
-    level=LOGGING_LEVEL,
-    format=(
-        "[%(asctime)s][%(levelname)s][%(name)s][%(funcName)s]"
-        + "[%(filename)s:%(lineno)d] %(message)s"
-    ),
-    datefmt="%Y-%m-%d %H:%M:%S",
+LOGGING_FORMAT = (
+    "[%(asctime)s][%(levelname)s][%(name)s][%(funcName)s]"
+    + "[%(filename)s:%(lineno)d] %(message)s"
 )
+
+# Create a logger
+logger = logging.getLogger()
+logger.handlers = []
+
+# Add console handler with INFO level
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
+logger.addHandler(console_handler)
+
+# Add file handler with DEBUG level
+file_handler = logging.FileHandler("gpt4docs.debug.log")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
+logger.addHandler(file_handler)
+
+logger.setLevel(logging.DEBUG)
 
 # Ignore logs from packages
 logging.getLogger("urllib3").setLevel(logging.WARNING)
