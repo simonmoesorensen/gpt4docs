33041dac8eb12c19d0bafc3251a81f56bc6c4f8e
Simon Moe Sørensen <sso@2021.ai>
2023-08-15 15:31:45 +0200

    Bump version: 0.1.2 → 0.1.3

--- gpt4docs/__init__.py
+++ gpt4docs/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Simon Moe Sørensen"""
 __email__ = "moe.simon@gmail.com"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from gpt4docs.modules import ProjectManager, VectorStoreManager, LLMManager
 from gpt4docs.modules.datamodels.PyDefinition import PyDefinition, PyDefinitionTypeEnum
