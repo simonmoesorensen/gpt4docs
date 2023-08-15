3e04ce2b00da64e7cedfb66469db35dc7d1d489d
Simon Moe Sørensen <sso@2021.ai>
2023-08-15 15:21:13 +0200

    ✨ feat(main): Call app as a callable rather than directly calling `run`

--- gpt4docs/__main__.py
+++ gpt4docs/__main__.py
@@ -1,5 +1,4 @@
 import gpt4docs.logger_config  # noqa: F401
-import asyncio
 from dotenv import load_dotenv
 from gpt4docs import MainApplication
 
@@ -9,4 +8,4 @@ load_dotenv()
 if __name__ == "__main__":
     args = MainApplication.parse_args()
     app = MainApplication(args)
-    asyncio.run(app.run())
+    app()
